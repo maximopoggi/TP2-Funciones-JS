6453e46f35540be3c8ec9876337d0eec7ad735f2
maximo poggi <maximogpoggi@gmail.com>
2026-06-14 22:41:18 -0300

    Update punto 7.js

--- punto 7.js
+++ punto 7.js
@@ -1,20 +1,20 @@
-function convertirADolares(pesos) {
+function Dolares(pesos) {
     return pesos / 1300; 
 }
 
-function convertirAPesos(dolares) {
+function Pesos(dolares) {
     return dolares * 1300; 
 }
 
 let monto = parseInt(prompt("Ingrese un monto:"));
-let opcion = parseInt(prompt("Ingrese 1 para convertir a dólares o 2 para convertir a pesos:"));
+let opcion = parseInt(prompt("Ingrese 1 para convertir a dólares o 2 para pesos:"));
 
 if (opcion === 1) {
-    alert("Monto en dólares: " + convertirADolares(monto));
-    console.log("Monto en dólares:", convertirADolares(monto));
+    alert("Monto en dólares: " + Dolares(monto));
+    console.log("Monto en dólares:", Dolares(monto));
 } else if (opcion === 2) {
-    alert("Monto en pesos: " + convertirAPesos(monto));
-    console.log("Monto en pesos:", convertirAPesos(monto));
+    alert("Monto en pesos: " + Pesos(monto));
+    console.log("Monto en pesos:", Pesos(monto));
 } else {
     alert("Opción incorrecta");
 }
